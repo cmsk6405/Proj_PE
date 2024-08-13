b9f9cbc23984614ec1133df681914cb2b6e8813f
kunsu <cmsk6405@naver.com>
2024-08-13 13:59:19 +0900

    frame skip

--- pose_project/img_webcam_compare.py
+++ pose_project/img_webcam_compare.py
@@ -6,10 +6,16 @@ from util.pose_compare import PoseCompare
 
 import numpy as np
 
+import torch
+
+
+
+import os
+os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
 
 def create_model():
 
-	yolo = YOLO('yolov8l-pose.pt')
+	yolo = YOLO("yolov8n.pt")
 	mp_pose = mp.solutions.pose
 	pose = mp_pose.Pose(static_image_mode=False, min_detection_confidence=0.6, min_tracking_confidence=0.6)
 
@@ -35,6 +41,8 @@ def main():
 	video_models = create_model()
 	webcam_models = create_model()
 
+	skip_frames = 3
+	frame_count = 0
 
 	while True:
         # Read from video
@@ -45,32 +53,35 @@ def main():
 			vid.set(cv2.CAP_PROP_POS_FRAMES, 0)
 			continue
 
-        # Inference video image
-		pose.load_img(frame=vid_frame, model=video_models, dest="ref") 
+		if frame_count % skip_frames == 0:
+	        # Inference video image
+			pose.load_img(frame=vid_frame, model=video_models, dest="ref") 
 
-        # Read from webcam
-		cam_ret, cam_frame = cam.read()
-		# cam flip
-		cam_frame = cv2.flip(cam_frame, 1)
-        # Inference webcam image
-		if not cam_ret:
-			print("Can't receive frame (stream end?). Exiting ...")
-			break
-		pose.load_img(frame=cam_frame, model=webcam_models, dest="trgt")
+			# Read from webcam
+			cam_ret, cam_frame = cam.read()
+			# cam flip
+			cam_frame = cv2.flip(cam_frame, 1)
+			# Inference webcam image
+			if not cam_ret:
+				print("Can't receive frame (stream end?). Exiting ...")
+				break
+			pose.load_img(frame=cam_frame, model=webcam_models, dest="trgt")
 
-		# pose counting
-		pose.counting()
-		# pose compare
-		pose.compare(offset=20)
+			# # pose counting
+			pose.counting()
+			# # pose compare
+			pose.compare(offset=20)
 
-		# frame 사이즈 조정
-		vid_frame = cv2.resize(vid_frame, (960 , 720))
-		cam_frame = cv2.resize(cam_frame, (960, 720))
-        # 비디오와 웹캠 합치기
-		hcombine_frame = np.hstack((vid_frame,cam_frame))
-		
-		# Show Result
-		cv2.imshow("Compare", hcombine_frame)
+			# frame 사이즈 조정
+			vid_frame = cv2.resize(vid_frame, (960 , 720))
+			cam_frame = cv2.resize(cam_frame, (960, 720))
+			# 비디오와 웹캠 합치기
+			hcombine_frame = np.hstack((vid_frame,cam_frame))
+			
+			# Show Result
+			cv2.imshow("Compare", hcombine_frame)
+
+		frame_count += 1
 
         # Breakaway condition
 		if cv2.waitKey(1) & 0xFF == ord('q'):
